20a01f5046aa96d5b9fced4aad149a00069848d4
Paul Belt <saprb@channing.harvard.edu>
2013-05-13 12:30:29 -0400

    ruby-1.9.x hash-style

--- test/alpha_numeric_validator_test.rb
+++ test/alpha_numeric_validator_test.rb
@@ -8,7 +8,7 @@ else
   raise RuntimeError, "Try ruby -Ilib test/#{File.basename(__FILE__)}"
 end
 
-ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ':memory:')
 
 ActiveRecord::Schema.define(:version => 1) do
   create_table :models do |t|
@@ -24,11 +24,11 @@ end
 class Model < ActiveRecord::Base
   attr_accessible :name, :organization, :favorite_quote, :server_name, :website, :id_code
   validates :name, :alpha_numeric => true
-  validates :organization, :alpha_numeric => { :punctuation => :limited }
-  validates :favorite_quote, :alpha_numeric => { :punctuation => true, :allow_nil => true, :allow_blank => true }
-  validates :server_name, :alpha_numeric => { :punctuation => :dns }
-  validates :website, :alpha_numeric => { :punctuation => :fqdn }
-  validates :id_code, :alpha_numeric => { :allow_whitespace => false }
+  validates :organization, :alpha_numeric => {punctuation: :limited}
+  validates :favorite_quote, :alpha_numeric => {punctuation: true, allow_nil: true, allow_blank: true}
+  validates :server_name, :alpha_numeric => {punctuation: :dns}
+  validates :website, :alpha_numeric => {punctuation: :fqdn}
+  validates :id_code, :alpha_numeric => {allow_whitespace: false}
 end
 
 class AlphaNumericValidatorTest < Test::Unit::TestCase
@@ -50,12 +50,12 @@ class AlphaNumericValidatorTest < Test::Unit::TestCase
     @model = Model.new(:name => 'Paul Belt', :organization => 'Hello Corp.', :favorite_quote => 'Hello world!',
       :server_name => 'hello-world', :website => 'hello-world.example.com', :id_code => 'abc123')
     assert @model.valid?
-    validations = {:name => [['Bob1 Jones2', 'abcdef'], ['Bob# Jones?', '!@#$!@']],
-      :organization => [["McDonald's", 'Yahoo!', 'ABC-DEF + GHI'], ['A@B', 'XYZ~3']],
-      :favorite_quote => [['This$is=a& good*string', '~!@#$%^&*()'], ["Hello\x00"]],
-      :server_name => [['hello1', 'abcdef'], ['hello world', 'hello~world']],
-      :website => [['abc123.example.com', 'example.com'], ['he llo.example.com', 'my@site.com']],
-      :id_code => [['hello1', 'abcdef'], ['hello world', 'hello-world']]}
+    validations = {name: [['Bob1 Jones2', 'abcdef'], ['Bob# Jones?', '!@#$!@']],
+      organization: [["McDonald's", 'Yahoo!', 'ABC-DEF + GHI'], %w(A@B XYZ~3)],
+      favorite_quote: [['This$is=a& good*string', '~!@#$%^&*()'], %W(Hello\x00)],
+      server_name: [%w(hello1 abcdef), ['hello world', 'hello~world']],
+      website: [%w(abc123.example.com example.com), ['he llo.example.com', 'my@site.com']],
+      id_code: [%w(hello1 abcdef), ['hello world', 'hello-world']]}
     validations.each do |k, v|
       validate_field(@model, k, v[0], v[1])
     end
